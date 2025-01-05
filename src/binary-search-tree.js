8a393493309f3f7c96f3109bd2bc59f0f8a4d040
Nadziya Frolova <sunny1976@mail.ru>
2025-01-06 03:55:55 +0400

    feat: solve some methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,43 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.rootNode = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.rootNode) {
+      this.rootNode = newNode;
+    } else {
+      this.specialNode(this.rootNode, newNode);
+    }
+  }
+  
+  specialNode(nod, newNode) {
+    if (newNode.data < nod.data) {
+      if (nod.left === null) {
+        nod.left = newNode;
+      } else {
+        this.specialNode(nod.left, newNode);
+      }
+    } else {
+      if (nod.right === null) {
+        nod.right = newNode;
+      } else {
+        this.specialNode(nod.right, newNode);
+      }
+    }
+  }
+
+  root() {
+    return this.rootNode;
   }
 
   has(/* data */) {
@@ -23,7 +45,7 @@ class BinarySearchTree {
     // remove line with error and write your code here
   }
 
-  find(/* data */) {
+  find(/*data*/) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
